2c4e52d2a8882ed6e2cc79365f37126d4dff0c9c
amanapp <137894561+amanapp@users.noreply.github.com>
2023-09-11 15:42:06 +0530

    Delete src directory

--- src/database/redis.ts
+++
@@ -1,5 +0,0 @@
-import { logger } from "../middleware/winsdon.middleware";
-import { createClient } from "redis";
-export const client = createClient()
-client.on("error", (err) => logger.log({level:"error",message:"Redis Client Error"}));
-client.connect();
